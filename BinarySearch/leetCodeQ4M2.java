9e6d2339e6b1630ad0b3142ea549ba0caf0a6f6e
Chandrakant Krishna Mane <mchandrakant554@gmail.com>
2024-09-30 22:15:35 +0530

    30-Sep-2024 sloved leetcode Qno. 4 most optimized version

--- BinarySearch/leetCodeQ4M2.java
+++ BinarySearch/leetCodeQ4M2.java
@@ -1,45 +1,45 @@
 package BinarySearch;
 
 public class leetCodeQ4M2 {
-    public static void print(int [] arr){
-        for(int ele : arr){
-            System.out.print(ele + " ");
-        }
-        System.out.println();
-    }
-    public static void swap(int i , int j , int[] nums1 , int[] nums2){
-        int temp = nums1[i] ;
-        nums1[i] = nums2[j] ;
-        nums2[j] = temp ;
-    }
-    public static void main(String[] args) {
-        int[] nums1 = {1,3,4,7,10,12};
-        int[] nums2 = {2,3,6,15} ;
+    public static double ans(int[] nums1 , int[] nums2){
         int n = nums1.length;
         int m = nums2.length;
-        int i = 0 , j = 0 ;
-        while(i<n && j < m){
-            if(nums1[i] <= nums2[j]) i++ ;
-            else if(nums1[i] >= nums2[j]){
-                swap(i , j , nums1 , nums2) ;
-                i++ ;
+        int left = (n + m + 1) / 2;
+        if(n > m) return ans(nums2 , nums1) ;
+        int lo = 0 , hi = n ;
+        System.out.println(hi);
+        while (lo <= hi) {
+            int mid = lo + (hi - lo)/2 ;
+            int mid2 = left - mid ;
+            int mid1 = mid ;
+            System.out.println("mid is " + mid);
+            int l1 = Integer.MIN_VALUE , l2 = Integer.MIN_VALUE ;
+            int r1 = Integer.MAX_VALUE , r2 = Integer.MAX_VALUE ;
+            if(mid1 < n)  r1 = nums1[mid1] ;
+            if(mid2 < m)  r2 = nums2[mid2] ; 
+            if(mid1 - 1 >= 0 ) l1 = nums1[mid1 - 1]  ;
+            if(mid2 - 1 >= 0 ) l2 = nums2[mid2 - 1] ;
+            
+            if(l1 <= r2 && l2 <= r1){
+                if((m+n)%2 == 0){
+                   return (double)(Math.max(l1,l2) + Math.min(r1,r2)) / 2.0 ;
+                } 
+                else{
+                    return (Math.max(l1, l2));
+                }
             }
+            else if(l1 > r2) hi = mid1 - 1;
+            else lo = mid1 + 1 ;    
         }
-        print(nums1);
-        print(nums2);
-        int x = m+n ;
-        if(x%2 == 0){
-            if(x/2 < n )
-                System.out.println((nums1[(x/2)-1] + nums1[(x/2)]) / 2);
-            else if(x/2 >= n)
-                System.out.println((nums2[(x/2)-n] + nums2[(x/2)-n+1]) / 2);
-            // else if(x/2 == n)
-            //     System.out.println((nums1[n-1]) + (nums2[(x/2 )-n]) / 2) ;
-        }
-        else{
-            if(x/2 <= n-1) System.out.println(nums1[x/2]);
-            else if(x/2 >= n) System.out.println(nums2[(x/2) - n]);
-        }
-        
+        return - 1;
+
+    }
+    
+
+    public static void main(String[] args) {
+        int[] nums2 = { 1, 3, 4, 7, 10, 12 };
+        int[] nums1 = { 2, 3, 6, 15 };
+        System.out.println(ans(nums1 ,nums2));
+
     }
 }
