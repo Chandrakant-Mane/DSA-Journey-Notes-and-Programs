35733c4724a570183e84031f966e881a0b0d684a
Chandrakant Krishna Mane <mchandrakant554@gmail.com>
2024-10-06 22:37:31 +0530

    this file modified little bit

--- Linked_List/leetCodeQ237.java
+++ Linked_List/leetCodeQ237.java
@@ -3,18 +3,25 @@ package Linked_List;
 class ListNode {
     int val;
     ListNode next;
-    ListNode node ;
     ListNode head ;
     ListNode(int x) { val = x; }
+
+    
 }
 
-public class leetCodeQ237 {
-    public static void deleteNode(ListNode node) {
-        node.val = node.next.val ;
-        node.next = node.next.next ;    
+class Solution{
+    ListNode head ;
+    ListNode tail ;
+    ListNode node ;
+    void insertAtTail(int val){
+        ListNode temp = new ListNode(val) ;
+        if(head == null) head = tail = temp ;
+        else{
+            tail.next = temp ;
+            tail = temp ;
+        }
     }
-    public static void display(ListNode a){
-        ListNode head = a;
+    void display(){
         ListNode temp = head ;
         while (temp != null) {
             System.out.print(temp.val + " ");
@@ -22,16 +29,32 @@ public class leetCodeQ237 {
         }
         System.out.println();
     }
+    ListNode traverse(){
+        ListNode temp = head;
+        while (temp != null) {
+            if(temp.val == 5)  return temp ;
+            temp = temp.next ;
+        }
+        return temp ;
+    }
+    void deleteNode(ListNode node) {
+        node.val = node.next.val ;
+        node.next = node.next.next ;    
+    }
+}
+
+public class leetCodeQ237 {
     public static void main(String[] args) {
-        ListNode a = new ListNode(4);
-        ListNode node = new ListNode(5);
-        ListNode b = new ListNode(1);
-        ListNode c = new ListNode(9);
-        a.next = node ;
-        node.next = b;
-        b.next = c ;
-        deleteNode(node) ;
-        display(a) ;
+        Solution list = new Solution();
+        list.insertAtTail(4);
+        list.insertAtTail(5);
+        list.insertAtTail(1);
+        list.insertAtTail(9);
+
+        list.display(); 
+        ListNode node = list.traverse();
+        list.deleteNode(node);
+        list.display();
 
     }
     
