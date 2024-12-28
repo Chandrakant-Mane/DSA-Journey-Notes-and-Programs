3225c418e4ab15ca265bac11d04416d5054270fb
Chandrakant Krishna Mane <mchandrakant554@gmail.com>
2024-12-28 22:48:00 +0530

    28-Dec-2024 Todays all programs were uploded

--- SlidingWindows/leetCodeQ1493.java
+++ SlidingWindows/leetCodeQ1493.java
@@ -4,10 +4,20 @@ public class leetCodeQ1493 {
     public static void main(String[] args) {
         int[] arr = {0,1,1,1,0,1,1,0,1} ;
         int n = arr.length, i = 0, j = 0;
+        int z = 0;
+        for (int ele : arr) {
+            if (ele == 0)
+                z++;
+        }
+        if (z == 0)
+            System.out.println(n-1);
+
         int zeroes = 0, maxLen = 0;
         while (i < n && arr[i] == 0) {
             i++;
         }
+        if (i == n)
+           System.out.println(0);
         j = i;
         while (j < n) {
             if (arr[j] == 1)
@@ -26,12 +36,10 @@ public class leetCodeQ1493 {
                 }
             }
         }
-        if (zeroes == 1) {
-            int len = j - i - 1;
-            maxLen = Math.max(maxLen, len);
-        }
         if (zeroes == 0)
-            System.out.println(n-1);
+            System.out.println(j-i);
+        int len = j - i - 1;
+        maxLen = Math.max(maxLen, len);
         System.out.println(maxLen);
     }
 }
