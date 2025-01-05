688b6faf5e4999ac0bf6e0b8483a27ca082780a6
Chandrakant Krishna Mane <mchandrakant554@gmail.com>
2025-01-05 23:03:03 +0530

    5-Jan-2025 Todays all programs were uploaded

--- DynamicProgramming/leetCodeQ198Memo.java
+++ DynamicProgramming/leetCodeQ198Memo.java
@@ -15,10 +15,13 @@ public class leetCodeQ198Memo {
         return Math.max(take, skip);
     }
     public static void main(String[] args) {
-        int[] nums = { 2, 7, 9, 3, 1 } ;
+        int[] nums = { 2 , 15 , 6 , 20 , 22 , 1 } ;
         int n = nums.length ;
         int[] dp = new int[n] ; //  'i' varies from 0 to n-1 ;
         Arrays.fill(dp,-1) ;
         System.out.println(amount(nums, 0, dp));
+        for(int ele : dp){
+            System.out.print(ele + " ");
+        }
     }
 }
--- DynamicProgramming/leetCodeQ509Tabulation.java
+++ DynamicProgramming/leetCodeQ509Tabulation.java
@@ -1,7 +1,8 @@
 package DynamicProgramming ;
 
 public class leetCodeQ509Tabulation {
-
+    
+    // SC :- O(n)
     public static int fib(int n) {
         if (n <= 1)
             return n;
@@ -13,7 +14,24 @@ public class leetCodeQ509Tabulation {
         }
         return dp[n];
     }
+
+    // Most Optimized  SC :- O(1) ;
+    public static int fib1(int n) {
+        if (n <= 1)
+            return n;
+        int[] dp = new int[3];
+        dp[0] = 0;
+        dp[1] = 1;
+        for (int i = 1; i <= n - 1; i++) {
+            dp[2] = dp[0] + dp[1];
+            dp[0] = dp[1];
+            dp[1] = dp[2];
+        }
+        return dp[2];
+    }
+    
     public static void main(String[] args) {
         System.out.println(fib(4));
+        System.out.println(fib1(8));
     }
 }
