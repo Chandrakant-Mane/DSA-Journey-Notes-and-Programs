cfddbdc55b89211228ae2e5dfff0b616edad5f48
Chandrakant Krishna Mane <mchandrakant554@gmail.com>
2025-01-15 23:40:01 +0530

    15-Jan-2025 Todays all programs were uploded

--- DynamicProgramming/leetCodeQ1143.java
+++ DynamicProgramming/leetCodeQ1143.java
@@ -1,6 +1,7 @@
 package DynamicProgramming ;
 
 public class leetCodeQ1143 {
+
     //      Normal Recursion 
     public static int lcs(int i, int j, StringBuilder a, StringBuilder b) {
         if (i < 0 || j < 0)
@@ -22,6 +23,42 @@ public class leetCodeQ1143 {
         else
             return dp[i][j] = Math.max(lcs(i, j - 1, a, b, dp), lcs(i - 1, j, a, b, dp));
     }
+    
+    //              JUST CONVERT MEMOIZATION TO TABULATION .
+    //      Tabulation ( Avoiding base Cases by creating (m+1 , n+1) dp)
+    public static int longestCommonSubsequence(String a, String b) {
+        int m = a.length(), n = b.length();
+        // i = m-1 to 0 | j = n-1 to 0
+        int[][] dp = new int[m + 1][n + 1];
+        for (int i = 1; i < dp.length; i++) {
+            for (int j = 1; j < dp[0].length; j++) {
+                if (a.charAt(i - 1) == b.charAt(j - 1))
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                else
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+        return dp[m][n];
+    }
+    //         Space Optimization .
+    public static int longestCommonSubsequence1(String a, String b) {
+        int m = a.length(), n = b.length();
+        // i = m-1 to 0 | j = n-1 to 0
+        int[][] dp = new int[2][n + 1];
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (a.charAt(i - 1) == b.charAt(j - 1))
+                    dp[1][j] = 1 + dp[0][j - 1];
+                else
+                    dp[1][j] = Math.max(dp[1][j - 1], dp[0][j]);
+            }
+            // Copy the 1st row to 0th row for next time
+            for (int j = 0; j <= n; j++) {
+                dp[0][j] = dp[1][j];
+            }
+        }
+        return dp[1][n];
+    }
     public static void main(String[] args) {
         String text1 = "abcde" ;
         String text2 = "ace" ;
@@ -40,6 +77,11 @@ public class leetCodeQ1143 {
         }
 
         System.out.println(lcs(m-1, n-1, a, b , dp));
+
+        System.out.println(longestCommonSubsequence(text1, text2));
+
+        System.out.println(longestCommonSubsequence1(text1, text2));
+
 
     }
 }
