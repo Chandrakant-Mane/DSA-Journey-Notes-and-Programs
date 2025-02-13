7098d33733a405b4c0c7b8dc86e087a5b7980028
Chandrakant Krishna Mane <mchandrakant554@gmail.com>
2025-02-13 23:22:51 +0530

    13-Feb-2025 Todays all programs were uploaded

--- Graph/leetCodeQ207.java
+++ Graph/leetCodeQ207.java
@@ -36,10 +36,50 @@ public class leetCodeQ207 {
         }
         return (ans.size() == n) ;
     }
+
+    public static boolean dfs(int i, List<List<Integer>> adj, boolean[] vis, boolean[] path, boolean ans) {
+        vis[i] = true;
+        path[i] = true;
+        for (int ele : adj.get(i)) {
+            if (path[ele] == true) {
+                ans = false;
+                return ans;
+            }
+            if (vis[ele] == false)
+                ans = dfs(ele, adj, vis, path, ans);
+        }
+        path[i] = false;
+        return ans;
+    }
+
+    public static boolean canFinishDfs(int n, int[][] pre) {
+        List<List<Integer>> adj = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            adj.add(new ArrayList<>());
+        }
+        // int[] indegree = new int[n] ; // Not required for DFS
+        for (int i = 0; i < pre.length; i++) {
+            int a = pre[i][0], b = pre[i][1]; // b -> a
+            // indegree[a]++ ;
+            adj.get(b).add(a);
+        }
+
+        // DFS Part :
+        boolean[] vis = new boolean[n];
+        boolean[] path = new boolean[n];
+        boolean ans = true; // true means NO Cycle
+        for (int i = 0; i < n; i++) {
+            if (vis[i] == false)
+                ans = dfs(i, adj, vis, path, ans);
+        }
+        return ans;
+    }
     public static void main(String[] args) {
         int n = 5 ;
         int[][] pre = { { 3, 2 } , { 1, 0 } , { 2, 1 } , { 4, 3 } , { 0, 3 } } ;
         System.out.println(canFinish(n, pre));
+
+        System.out.println(canFinishDfs(n, pre));
 
     }
 }
---
+++ Graph/leetCodeQ841.java
@@ -0,0 +1,60 @@
+package Graph ;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class leetCodeQ841 {
+    public static void bfs(int start , boolean[] vis , List<List<Integer>> adj){
+        Queue<Integer> q = new LinkedList<>() ;
+        q.add(start) ;
+        while(q.size()>0){
+            int front = q.remove() ;
+            for(int ele : adj.get(front)){
+                if(!vis[ele]){
+                    q.add(ele) ;
+                    vis[ele] = true ;
+                }
+            }
+        }
+    }
+
+    public static void dfs(int start , boolean[] vis , List<List<Integer>> adj){
+        vis[start] = true ;
+        for(int ele : adj.get(start)){
+            if(!vis[ele]){
+                dfs(ele , vis , adj) ;
+            }
+        }
+    }
+
+    public static boolean canVisitAllRooms(List<List<Integer>> adj) {
+        int n = adj.size() ;
+        boolean[] vis = new boolean[n] ;
+        // vis[0] = true ;
+        // bfs(0 , vis , adj) ;
+        dfs(0 , vis , adj) ;
+        for(boolean ele : vis){
+            if(ele == false)
+                return false ;
+        }
+        return true ;
+    }
+    public static void main(String[] args) {
+        List<List<Integer>> adj = new ArrayList<>() ;
+
+        List<Integer> a = new ArrayList<>() ;
+        a.add(1) ;  a.add(3) ;
+        List<Integer> b = new ArrayList<>() ;
+        b.add(3) ;  b.add(0) ;  b.add(1) ;
+        List<Integer> c = new ArrayList<>() ;
+        c.add(2) ;
+        List<Integer> d = new ArrayList<>() ;
+        d.add(0) ;
+
+        adj.add(a) ; adj.add(b) ; adj.add(c) ; adj.add(d) ;
+
+        System.out.println(canVisitAllRooms(adj));
+    }
+}
